54f27ec0d1ab7b3a95c6bb6c5cf0de83a25b78e2
Lodomo <lodomo.dev@gmail.com>
2025-03-28 21:16:12 -0700

    Added dal's API endpoints

--- web_if/api/app.py
+++ web_if/api/app.py
@@ -29,8 +29,84 @@ def info():
     result = call_device("--info")
     device_info = result.stdout.decode().strip()
     info_json = json.loads(device_info.replace("'", '"'))
-
     return info_json
+
+
+# Created these paths based on Dal's design to make them interchangable with the device frontend.
+
+# Use POST to upload files
+
+
+@app.route("/img/local/<filename>")
+def img_local():
+    """
+    Upload a file to the device
+    """
+    pass
+
+
+@app.route("/img/remote/<filename>")
+def img_remote():
+    """
+    Download a file to device from remote server
+    """
+    pass
+
+
+@app.route("/pi/health")
+def pi_health():
+    """
+    Get the health of the device
+    """
+    return info()
+
+
+@app.route("/pi/os")
+def pi_os():
+    """
+    Update the OS of the device
+    """
+    pass
+
+
+@app.route("/pi/vbd")
+def pi_vbd():
+    """
+    Update the VBD of the device
+    """
+    pass
+
+
+@app.route("/pi/pin/<old>/<new>")
+def pi_pin():
+    """
+    Update the pin of the device
+    """
+    pass
+
+
+@app.route("/pi/auth")
+def pi_auth():
+    """
+    User login
+    """
+    pass
+
+
+@app.route("/pi/mode/<filename_or_off>")
+def pi_mode():
+    """
+    Set the device ISO mode
+    """
+    pass
+
+
+@app.route("/pi/fd/")
+def pi_fd():
+    """
+    Toggle the device flash drive mode
+    """
+    pass
 
 
 def call_device(command, *args):
